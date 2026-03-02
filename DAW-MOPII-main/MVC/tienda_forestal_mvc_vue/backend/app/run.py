05daed50339ab61fb99ce33bdce06b4a2445775c
HuGoSanJuan18 <hugo.sanmor.1@educa.jcyl.es>
2026-03-02 17:27:21 +0100

    Primer commit - comentario quitado del dockerfile fila 23

---
+++ DAW-MOPII-main/MVC/tienda_forestal_mvc_vue/backend/app/run.py
@@ -0,0 +1,27 @@
+"""
+Punto de entrada principal del servidor Flask.
+Registra los Blueprints (controladores) y arranca la aplicación.
+"""
+
+from flask import Flask
+from flask_cors import CORS
+
+# Import del blueprint
+from controllers.producto_controller import producto_blueprint
+
+app = Flask(__name__)
+
+# Habilitar CORS
+CORS(app)
+
+# Registrar blueprint
+app.register_blueprint(producto_blueprint, url_prefix="/api")
+#with app.test_request_context():
+#    print(app.url_map)
+@app.route("/")
+def home():
+    return "Backend Flask funcionando correctamente."
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
+
